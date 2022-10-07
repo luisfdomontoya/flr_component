ace8d3b140b4d9d6c0709ed2cd6796fe8da7461d
Luis Fernando Montoya <luisfdomontoya@outlook.com>
2022-10-07 08:59:12 -0500

    card -- card widget part 3 -- adding theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Colors.green;
+  static const Color primary = Colors.indigo;
 
   //Para el tema light
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      //color primario
-      primaryColor: primary,
-      appBarTheme: const AppBarTheme(
-        color: primary,
-        elevation: 0,
-      ));
+    //Primary Color
+    primaryColor: primary,
+
+    //AppBar Theme: estos serían los estilos para todos mis appBar
+    appBarTheme: const AppBarTheme(
+      color: primary,
+      elevation: 0,
+    ),
+
+    //TextButton Theme: estos serían los estilos para todos mis appBar
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(foregroundColor: primary),
+    ),
+  );
 
   //Para el tema dark
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
