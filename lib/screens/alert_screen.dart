2eeeed727c6316ad3fe04d669ca9697700438ffa
Luis Fernando Montoya <luisfdomontoya@outlook.com>
2022-10-08 11:10:20 -0500

    modal -- alert screen -- modal android version

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,31 +3,53 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialogAndroid(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            title: const Text('Alert Title'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('I\'m an Alert!'),
+                SizedBox(height: 10),
+                FlutterLogo(size: 100),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancel'),
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          // style: ElevatedButton.styleFrom(
-          //     backgroundColor: Colors.indigo,
-          //     shape: const StadiumBorder(),
-          //     elevation: 0),
           child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 15,
+            ),
             child: Text(
               'Show Alert',
               style: TextStyle(fontSize: 16),
             ),
           ),
-          onPressed: () {},
+          onPressed: () => displayDialogAndroid(context),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
-        onPressed: () {
-          Navigator.pop(context); //muestra la última pantalla que está en el
-          //stack de cartas. En este caso sería el Home Screen.
-        },
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
