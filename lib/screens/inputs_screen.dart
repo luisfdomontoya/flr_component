b882e2a65574f7f300ee48aeb4ec8dca25b14c4e
Luis Fernando Montoya <luisfdomontoya@outlook.com>
2022-10-10 14:43:57 -0500

    Form - part I

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -6,6 +6,14 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Map<String, String> formValues = {
+      'first_name': 'Luis',
+      'last_name': 'Montoya',
+      'email': 'luismontoya@gmail.com',
+      'password': '123456',
+      'role': 'admin',
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs and Forms'),
@@ -16,36 +24,42 @@ class InputsScreen extends StatelessWidget {
             horizontal: 20,
             vertical: 10,
           ),
-          child: Column(
-            children: const [
-              CustomInputField(
-                labelText: 'Name',
-                hintText: 'Username',
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CustomInputField(
-                labelText: 'Lastname',
-                hintText: 'Lastname',
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CustomInputField(
-                labelText: 'Email',
-                hintText: 'correo@correo.co',
-                obscureText: true,
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CustomInputField(
-                labelText: 'Password',
-                hintText: 'Enter a password',
-                keyboardType: TextInputType.emailAddress,
-              ),
-            ],
+          child: Form(
+            child: Column(
+              children: [
+                const CustomInputField(
+                  labelText: 'Name',
+                  hintText: 'Username',
+                ),
+                const SizedBox(height: 30),
+                const CustomInputField(
+                  labelText: 'Lastname',
+                  hintText: 'Lastname',
+                ),
+                const SizedBox(height: 30),
+                const CustomInputField(
+                  labelText: 'Email',
+                  hintText: 'correo@correo.co',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 30),
+                const CustomInputField(
+                  labelText: 'Password',
+                  hintText: 'Enter a password',
+                  obscureText: true,
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(
+                      child: Text('Save'),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
